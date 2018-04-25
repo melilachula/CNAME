573a2b24a0305f2e716cff65a2db771f7ca07d8c
Melina Maldonado <m.maldonado@miami.edu>
2018-04-25 17:08:44 -0400

    Gallery fixes

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+/*$("#replacetext").hover(function(){
+$("#replace").html("<p>Now pick a partner and enjoy!</p>");
+});*/
+
+$(".replacetext").hover(function(){
+$("replace",).html("<p>Now pick a partner and enjoy!</p>");
+});
+
+});
