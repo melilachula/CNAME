c655e9c31504fc1f271b0ee86b440e8373d82e8d
Melina Maldonado <m.maldonado@miami.edu>
2018-04-25 19:55:32 -0400

    replace

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 $(document).ready(function(){
 
-/*$("#replacetext").hover(function(){
-$("#replace").html("<p>Now pick a partner and enjoy!</p>");
-});*/
-
-$(".replacetext").hover(function(){
-$("replace",).html("<p>Now pick a partner and enjoy!</p>");
-});
-
+  $("#replacetext").hover(function(){
+  $("#replace").html("<p>Maybe this will help.</p>");
+  });
+  
 });
